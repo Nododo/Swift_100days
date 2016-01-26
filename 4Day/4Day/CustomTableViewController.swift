128c4491c04cb24b9b6be5954c38359b9a3e3e71
DWX <nododo1027@gmail.com>
2016-01-26 18:21:23 +0800

    4-day
    commit the 4th day file
    the 3-day 4-day cost me a total day

---
+++ 4Day/4Day/CustomTableViewController.swift
@@ -0,0 +1,106 @@
+//
+//  CustomTableViewController.swift
+//  4Day
+//
+//  Created by 杜维欣 on 16/1/26.
+//  Copyright © 2016年 Nododo. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewController: UITableViewController {
+    
+    private var localData: [String] = ["IronMan", "SpiderMan", "BatMan"]
+    
+    @IBAction func editMe(sender: UIBarButtonItem) {
+        UITableViewController
+        self.editing = !self.editing;
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Movies"
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return localData.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("nododo", forIndexPath: indexPath)
+        if let tempLabel = cell.textLabel {
+            tempLabel.text  = localData[indexPath.row]
+        }
+
+        return cell
+    }
+    
+
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+
+    
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            localData.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+
+    
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+    
+        let itemToMove = localData[fromIndexPath.row]
+        localData.removeAtIndex(fromIndexPath.row)
+        localData.insert(itemToMove, atIndex: toIndexPath.row)
+    }
+    
+
+    
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
