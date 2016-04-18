8004282a89efe37744c5a0091102dcf4e7330aed
DWX <nododo1027@gmail.com>
2016-04-18 18:23:58 +0800

    commit 36Day

---
+++ 36Day/36Day/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  36Day
+//
+//  Created by 杜维欣 on 16/4/18.
+//  Copyright © 2016年 Nododo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+                                                                              }
+
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
+
