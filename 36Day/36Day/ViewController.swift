06b1ff55b3a3c68a49256731b36c0ef156f3e50d
DWX <nododo1027@gmail.com>
2016-04-26 11:56:10 +0800

    complete 36Day

--- 36Day/36Day/ViewController.swift
+++ 36Day/36Day/ViewController.swift
@@ -8,27 +8,54 @@
 
 import UIKit
 
+// MARK: 在这里可以学到更多的东西 https://github.com/callumboddy/CBZSplashView
+
 class ViewController: UIViewController {
 
+    var iconView: UIImageView!
+    var animationStop = false
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        UIApplication.sharedApplication().setStatusBarHidden(true, withAnimation: .None);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(self.coverView)
-        // Do any additional setup after loading the view, typically from a nib.
+        self.doAnimation()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-                                                                              }
-
-
-    override func prefersStatusBarHidden() -> Bool {
-        return true
+    func doAnimation() {
+        let animation = CAKeyframeAnimation.init(keyPath: "transform.scale")
+        animation.values = [1, 0.9, 15]
+        animation.keyTimes = [0, 0.4, 1]
+        animation.duration = 1;
+        animation.delegate = self
+        animation.removedOnCompletion = false
+        animation.fillMode = kCAFillModeForwards
+        animation.timingFunctions = [CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseOut), CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseIn)]
+        self.iconView.layer.addAnimation(animation, forKey: "scale")
     }
     
-
+    override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        self.coverView.removeFromSuperview()
+        self.animationStop = true
+        UIApplication.sharedApplication().setStatusBarHidden(false, withAnimation: .Fade);
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return false
+    }
+    
     lazy var coverView: UIView = {
-        var tempView = UIView.init(frame: self.view.bounds)
-        tempView.backgroundColor = UIColor.redColor()
+        var tempView: UIView = UIView.init(frame: self.view.bounds)
+        tempView.backgroundColor = UIColor.init(red: 93 / 255.0, green: 173 / 255.0, blue: 244 / 255.0, alpha: 1)
+        let iconView = UIImageView.init(image: UIImage(named: "twitterIcon"))
+        iconView.bounds.size = CGSizeMake(62, 50)
+        iconView.center = tempView.center
+        tempView.addSubview(iconView)
+        self.iconView = iconView
         return tempView
     }()
 }
